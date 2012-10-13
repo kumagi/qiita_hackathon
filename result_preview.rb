bc903205eeed9206b0cc4e4fb68b36e50d5adc4b
tomykaira <tomykaira@gmail.com>
2012-10-13 15:17:01 +0900

    Add color with Term::ANSIColor

--- result_preview.rb
+++ result_preview.rb
@@ -2,6 +2,9 @@ require 'qiita'
 require 'json'
 require 'redcarpet'
 require 'redcarpet/render_strip'
+require 'term/ansicolor'
+
+C = Term::ANSIColor
 
 # articles = ["d2893476d39b4d120c57","1a3fcfb9cdb60745fead","a4e494fef27e477ecad3","2fa140c93c9ef2b0a7c4","72ada26499a3c182b47a","b586818e60b8fc7ce68f","bf47e254d662af1294d8","c686397e4a0f4f11683d","e84f5aad7757afce82ba","f41b492203b6136ba77a"]
 
@@ -13,7 +16,7 @@ require 'redcarpet/render_strip'
 objects = JSON.parse(File.read("item_jsons.json"))
 
 objects.each do |d|
-  puts "%s [%3s] %s [ %s ]" % [d["user"]["url_name"].ljust(10), d["stock_count"], d["title"], "http://qiita.com/items/" + d["uuid"]]
+  puts "#{C.blue + C.bold}%s [%3s] %s [ %s ]#{C.reset}" % [d["user"]["url_name"].ljust(10), d["stock_count"], d["title"], "http://qiita.com/items/" + d["uuid"]]
   puts Redcarpet::Markdown.new(Redcarpet::Render::StripDown.new).render(d["raw_body"]).
     split("\n").
     take(5).
